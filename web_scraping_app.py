51df2f68cc6dc5324e4943f7bf0c7f3e11538279
HussnainArshad838 <126756076+HussnainArshad838@users.noreply.github.com>
2023-05-27 22:49:09 +0500

    Add files via upload

---
+++ web_scraping_app.py
@@ -0,0 +1,33 @@
+!pip install streamlit
+!pip install requests
+!pip install beautifulsoup4
+
+import requests
+from bs4 import BeautifulSoup
+import streamlit as st
+
+# Web scraping function
+def scrape_data(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # TODO: Add your scraping logic here
+
+    return scraped_data
+
+# Streamlit app code
+def main():
+    st.title("Web Scraping App")
+    st.write("Enter the URL to scrape:")
+    url = st.text_input("URL")
+
+    if st.button("Scrape"):
+        if url:
+            scraped_data = scrape_data(url)
+            st.write("Scraped data:")
+            st.write(scraped_data)
+        else:
+            st.write("Please enter a URL to scrape.")
+
+if __name__ == '__main__':
+    main()
