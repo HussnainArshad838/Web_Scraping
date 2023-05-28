295c85cf9092d89a59689780e31f1afc92ebeff5
HussnainArshad838 <126756076+HussnainArshad838@users.noreply.github.com>
2023-05-28 16:18:10 +0500

    web_scraping_app.py

--- web_scraping_app.py
+++ web_scraping_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-pip install beautifulsoup4
+#pip install beautifulsoup4
 from bs4 import BeautifulSoup
 import webbrowser
 
@@ -38,19 +38,6 @@ with st.form("Search"):
                 
                 st.image(image_list[0])
 
-#import streamlit as st
-#import requests
-#from bs4 import BeautifulSoup
-
-#st.markdown("<h1 style='text-align:center;'>Web Scraping</h1>", unsafe_allow_html=True)
-#with st.form("Search"):
-   # keyword = st.text_input("Enter Your Keyword")
-   # search = st.form_submit_button("Search")
-    
-  #  if search:
-   #     page = requests.get(f"https://unsplash.com/s/photos/{keyword}")
-    #    soup=Beautifulsoup(page.content,'lxml')
-    #    rows=soup.find_all("div",class_="ripi6")
 
 
 
