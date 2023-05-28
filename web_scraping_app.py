24fb423225f163453c6c329f4d0353de529429a2
HussnainArshad838 <126756076+HussnainArshad838@users.noreply.github.com>
2023-05-28 15:06:19 +0500

    web_scraping_app.py

--- web_scraping_app.py
+++ web_scraping_app.py
@@ -7,10 +7,30 @@ st.markdown("<h1 style='text-align:center;'>Web Scraping</h1>", unsafe_allow_htm
 with st.form("Search"):
     keyword = st.text_input("Enter Your Keyword")
     search = st.form_submit_button("Search")
-    
+
     if search:
         page = requests.get(f"https://unsplash.com/s/photos/{keyword}")
-        print(page.status_code)
+        soup = BeautifulSoup(page.content, 'html.parser')  # Changed parser to 'html.parser'
+        rows = soup.find_all("div", class_="_2VWD4 _2zEKz")  # Updated the class name
+
+        for row in rows:
+            # Perform further processing on each row as needed
+            pass
+
+#import streamlit as st
+#import requests
+#from bs4 import BeautifulSoup
+
+#st.markdown("<h1 style='text-align:center;'>Web Scraping</h1>", unsafe_allow_html=True)
+
+#with st.form("Search"):
+   # keyword = st.text_input("Enter Your Keyword")
+   # search = st.form_submit_button("Search")
+    
+  #  if search:
+   #     page = requests.get(f"https://unsplash.com/s/photos/{keyword}")
+    #    soup=Beautifulsoup(page.content,'lxml')
+    #    rows=soup.find_all("div",class_="ripi6")
 
 
 
