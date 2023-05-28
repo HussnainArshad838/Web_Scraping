dc7f7c4310105468ebbbfb748ae1b5e05dfe7afe
HussnainArshad838 <126756076+HussnainArshad838@users.noreply.github.com>
2023-05-28 14:48:41 +0500

    web_scraping_app.py

--- web_scraping_app.py
+++ web_scraping_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.markdown("<h1 style='text-align:center;'>Web Scraping </h1>",unsafe_allow_html=True)
 with st.form("Search"):
-keyword=st.text_input("Enter Your Keyword")
-st.form_submit_button("Search")
+    keyword=st.text_input("Enter Your Keyword")
+    st.form_submit_button("Search")
 
 
 
